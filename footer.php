7e1ebdf6326b20673632dcfb1552c12b9f8aec3f
Noushad Nipun <nipunnoushad8@gmail.com>
2020-07-22 17:51:45 +0600

    add hover on navbar show dropdown

--- footer.php
+++ footer.php
@@ -122,9 +122,8 @@
 	</section><!-- Footer -->
 </div>
 	<!--  JavaScript -->
-    <script ddsrc="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="assets/js/main.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="assets/js/main.js"></script>
   </body>
 </html>
